ca90b09abf8e9b39530119881e75d3e9244c4940
Marantesss <up201706917@fe.up.pt>
2020-02-27 12:09:18 +0000

    Finished LAB2 Server

--- lab2/Server.java
+++ lab2/Server.java
@@ -8,16 +8,14 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 /**
- * multicast: <mcast_addr> <mcast_port>: <srvc_addr> <srvc_port>
+ * java Server <srvc_port> <mcast_addr> <mcast_port> 
  *
- * <mcast_addr>
- *      is the IP address of the multicast group used by the server to advertise its service;
- *  <mcast_port>
- *      is the port number of the multicast group used by the server to advertise its service;
- * <srvc_addr>
- *      is the IP address where the server provides its service;
  * <srvc_port>
- *      is the port number where the server provides its service.
+ *      is the port number where the server provides the service
+ * <mcast_addr>
+ *      is the IP address of the multicast group used by the server to advertise its service.
+ * <mcast_port>
+ *      is the multicast group port number used by the server to advertise its service.
  */
 public class Server {
     
@@ -25,7 +23,7 @@ public class Server {
     private MulticastSocket multicastSocket;
     private int multicastPort;
     private InetAddress multicastAddress;
-    private Timer timer;
+    private Timer advertiserTimer;
 
     // DNS Service from LAB1
     private DatagramSocket datagramSocket;
@@ -44,18 +42,111 @@ public class Server {
         }
     }
 
-    private class AdvertiserTask extends TimerTask {
-        public void run() {
-            String portString = Integer.toString(datagramPort);
-            DatagramPacket packet = new DatagramPacket(portString.getBytes(), portString.getBytes().length, multicastAddress,
-                    multicastPort);
+    private void create(String[] args) throws IOException {
+        // Advertiser
+        this.multicastPort = Integer.parseInt(args[2]);
+        this.multicastAddress = InetAddress.getByName(args[1]);
+        this.multicastSocket = new MulticastSocket(this.multicastPort);
 
+        // DNS Service
+        this.datagramPort = Integer.parseInt(args[0]);
+        this.datagramSocket = new DatagramSocket(this.datagramPort);
+
+        // Data
+        this.table = new Hashtable<String, String>();
+
+        // Create and schedule timer's task
+        this.advertiserTimer = new Timer();
+        TimerTask advertiserTask = new AdvertiserTask();
+        this.advertiserTimer.schedule(advertiserTask, 0, 1000);
+    }
+
+    private void run() throws IOException {
+        byte[] buf = new byte[256];
+        DatagramPacket packet = new DatagramPacket(buf, buf.length);
+        this.datagramSocket.receive(packet);
+        this.process(packet);
+    }
+
+    private void process(DatagramPacket packet) throws IOException {
+        String received = new String(packet.getData());
+        
+        String message = this.parse(received);
+        
+        int result = -1;
+        
+        if (message != null) {
+            result = table.size();
+            
+            message = result + "\n" + message;
+        } else {
+            message = Integer.toString(result);
+        }
+
+        byte[] buf = message.getBytes();
+
+        int clientPort = packet.getPort();
+
+        InetAddress clientAddress = packet.getAddress();
+        
+        DatagramPacket newPacket = new DatagramPacket(buf, buf.length, clientAddress, clientPort);
+
+        this.datagramSocket.send(newPacket);
+    }
+
+    private String parse(String received) {
+        String[] tokens = received.split(" ");
+
+        String response;
+
+        for(int i=0; i<tokens.length; i++) {
+            tokens[i] = tokens[i].trim();
+            System.out.println(tokens[i]);
+        }
+        
+        if (tokens[0].equals("register")) {
+            if (this.table.containsKey(tokens[1]) == false) {
+                this.table.put(tokens[1], tokens[2]);
+                response = tokens[1] + " " + tokens[2];
+
+                System.out.println("register " + tokens[1] + " " + tokens[2] + " :: " + response);
+
+                return response;
+            } else {
+
+                return null;
+            }
+        } else if (tokens[0].equals("lookup")) {
+
+            if (this.table.containsKey(tokens[1]) == false) {
+                return null;
+            } else {
+                response = tokens[1] + " " + this.table.get(tokens[1]);
+
+                System.out.println("lookup " + tokens[1] + " :: " + response);
+                
+                return response;
+            }
+        } else {
+            System.out.println("Error parsing message");
+
+            return null;
+        }
+
+    }
+
+    private class AdvertiserTask extends TimerTask {
+        @Override
+        public void run() {
+            // build packet
+            byte[] buf = Integer.toString(datagramPort).getBytes();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, multicastAddress, multicastPort);
+
+            // send packet
             try {
                 multicastSocket.send(packet);
-                String multicastPrint = "multicast: " + packet.getAddress().toString() + " "  + packet.getPort() + ":" + datagramSocket.getLocalAddress() + " " + datagramSocket.getLocalPort();
-                System.out.println(multicastPrint);
-
-            } catch (IOException | InterruptedException e) {
+                System.out.println("multicast: " + packet.getAddress().toString() + " "  + packet.getPort() + ":" + datagramSocket.getLocalAddress() + " " + datagramSocket.getLocalPort());
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
