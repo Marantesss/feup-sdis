e3ac7a34cd1cddf0eaaede9ac5e28d46fd3578f9
César Nogueira <github@cesarnogueira.com>
2020-03-05 12:03:04 +0000

    Started implementing server

--- lab3/ServerInterface.java
+++ lab3/ServerInterface.java
@@ -1,8 +1,10 @@
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface ServerInterface extends Remote {
-
 	void register();
-
+	
 	void lookup();
+
+	String sayHello() throws RemoteException;
 }
